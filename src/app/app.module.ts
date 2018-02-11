3bc5733609d9a4674275e510395ff08711fc5a45
somq <mail.clement@gmail.com>
2018-02-11 21:22:16 +0100

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDACgTk49qpw3p1HLUSzOQspUXeQpE73fE",
+  authDomain: "ocp-app.firebaseapp.com",
+  databaseURL: "https://ocp-app.firebaseio.com",
+  projectId: "ocp-app",
+  storageBucket: "ocp-app.appspot.com",
+  messagingSenderId: "209501442130"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  currentUserAppProfiles: Observable<{}[]>;
+  currentUserPhones: Observable<{}[]>;
+
+  constructor(db: AngularFirestore) {
+    this.currentUserAppProfiles = db.collection('appProfiles', ref => ref.where('owner', '==', 'JpPfT3V1cFqNh44f54Dn')).valueChanges();
+
+    this.currentUserAppProfiles.subscribe(res => {
+      console.log(res)
+    })
+
+    this.currentUserPhones = db.collection('phones', ref => ref.where('owner', '==', 'JpPfT3V1cFqNh44f54Dn').where('test', '==', 'abcdx')).valueChanges();
+
+    this.currentUserPhones.subscribe(res => {
+      console.log('a', res)
+      // console.log(res[0].appProfile.path)
+
+      // this.currentUserPhones = db.collection(res[0].appProfile.path).valueChanges();
+
+      // this.currentUserPhones.subscribe(res => {
+      //   console.log(res)
+      // })
+
+    })
+
+
+
+
+  }
+
+}
